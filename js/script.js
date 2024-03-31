53af774db2b2c031a31426ca1cad220c25885cca
MatteoBarducci <mbardu1301@gmail.com>
2024-03-31 21:04:59 +0200

    feat: add bonus

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ createApp({
     data() {
         return {
             message: 'Messaggio',
+            url: 'https://www.freecodecamp.org/news/content/images/size/w2000/2022/08/Vue-Blog-Cover-2.png',
         };
     }
 }).mount('#app');
